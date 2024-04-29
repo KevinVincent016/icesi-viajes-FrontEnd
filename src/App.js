61d1c969964a8f4bfbb9934d124bf9499b7526d4
Juan181803 <jframirez.valencia@gmail.com>
2024-04-29 00:50:23 -0500

    Pantalla Login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import backgroundImage from '/Users/juanfelipe/Desktop/IcesiViajesFrontEndGithub/icesi-viajes-FrontEnd/src/backgroundimage.png';
+import logo from '/Users/juanfelipe/Desktop/IcesiViajesFrontEndGithub/icesi-viajes-FrontEnd/src/image-removebg-preview (1).png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header">
+        <div className="logo-container">
+          <img src={logo} alt="Logo" className="logo" />
+          <span className="company-name">ICESI VIAJES</span>
+        </div>
+        <span className="help-button">Ayuda</span>
+      </div>
+      <div className="App" style={{ backgroundImage: `url(${backgroundImage})` }}>
+        <div className="login-container">
+          <h1>Bienvenido</h1>
+          <form>
+            <input type="text" placeholder="Usuario" />
+            <input type="password" placeholder="Contraseña" />
+            <button type="submit">Iniciar sesión</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
